e210ed9c5d74508b6b2c72225e4fa4d314d9c0b7
Lukasz Pawelczyk <l.pawelczyk@samsung.com>
2025-08-22 14:02:33 +0200

    Use serial port instead of MHU for RSE comms
    The changes consist mostly of an object (rse_comms_serial.c)
    reimplementing MHU API using serial port and makefile modifications to
    link that object instead of MHU when proper define is set.
    
    Changes to rse_comms.c and mhu_v2_x.c consist only of debug messages and
    some required bugfixes.
    
    Signed-off-by: Lukasz Pawelczyk <l.pawelczyk@samsung.com>

--- drivers/arm/rse/rse_comms.mk
+++ drivers/arm/rse/rse_comms.mk
@@ -22,10 +22,17 @@ RSE_COMMS_SOURCES	+=	$(addprefix drivers/arm/mhu/,			\
 					mhu_wrapper_v3_x.c			\
 				)
 else ifeq (${PLAT_MHU_VERSION}, 2)
+ifneq (${PLAT_RSE_COMMS_USE_SERIAL},0)
+$(warning "RSE serial used, need an external HES implementation.")
+RSE_COMMS_SOURCES	+=	$(addprefix drivers/arm/rse/,			\
+					rse_comms_serial.c			\
+				)
+else
 RSE_COMMS_SOURCES	+=	$(addprefix drivers/arm/mhu/,			\
 					mhu_v2_x.c				\
 					mhu_wrapper_v2_x.c			\
 				)
+endif
 else
 $(error Unsupported MHU version)
 endif
