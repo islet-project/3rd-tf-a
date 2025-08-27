islet-project/arm-trusted-firmware

Workaround for hazard between launching HES and FVP
Once in a while the measurement messages sent by BL1
don't get replies, and the booting process stuck.
It seems that BL1 sends these messages before HES is
ready to handle them.

A small delay in the BL1 and HES startup code resolves the issue.

Proposal: Implement some kind of synchronization approach
between FVP and HES. The BL1 should send
a ping message and after some timeout send it again until
it gets a reply message from HES. Only then proceed with normal
execution.

Signed-off-by: Piotr Sawicki <p.sawicki2@samsung.com>

plat/arm/board/fvp/fvp_bl1_setup.c
@@ -18,6 +18,14 @@
 #include <plat/common/platform.h>
 #include "fvp_private.h"
 
+static inline void hes_delay_workaround()
+{
+	volatile unsigned int a = 0x5000000ul;
+	while (a != 0) {
+		a--;
+	};
+}
+
 /*******************************************************************************
  * Perform any BL1 specific platform actions.
  ******************************************************************************/
@@ -37,6 +45,13 @@ void bl1_early_platform_setup(void)
 	 * Enable coherency in Interconnect for the primary CPU's cluster.
 	 */
 	fvp_interconnect_enable();
+
+	/**
+	 * XXX Workaround
+	 * We need to wait a while for HES before
+	 * sending any PSA message via UART
+	 */
+	hes_delay_workaround();
 }
 
 void plat_arm_secure_wdt_start(void)

islet-project/3rd-tf-a

plat/arm/board/fvp/fvp_bl1_setup.c
@@ -18,6 +18,14 @@
 #include <plat/common/platform.h>
 #include "fvp_private.h"
 
+static inline void hes_delay_workaround()
+{
+	volatile unsigned int a = 0x15000000ul;
+	while (a != 0) {
+		a--;
+	};
+}
+
 /*******************************************************************************
  * Perform any BL1 specific platform actions.
  ******************************************************************************/
@@ -37,6 +45,13 @@ void bl1_early_platform_setup(void)
 	 * Enable coherency in Interconnect for the primary CPU's cluster.
 	 */
 	fvp_interconnect_enable();
+
+	/**
+	 * XXX Workaround
+	 * We need to wait a while for HES before
+	 * sending any PSA message via UART
+	 */
+	hes_delay_workaround();
 }
 
 void plat_arm_secure_wdt_start(void)